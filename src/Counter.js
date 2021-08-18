f4b639b0abb2795beff1a7abc512deebcf99a302
Priyabrat <priyabratswain888@gmail.com>
2021-08-18 22:48:45 +0530

    Create Counter.js file

---
+++ src/Counter.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+
+export default class Counter extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+           count: props.intialCount
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            <button onClick={() => this.changeCount(-1)}>-</button>
+            <span>{this.state.count}</span>
+            <button onClick={() => this.changeCount(1)}>+</button>
+          </div>
+        )
+    }
+
+    changeCount(amount) {
+        this.setState(prevState => { return {count: prevState.count + amount }})
+        // this.setState({count: this.state.count + amount})
+    }
+}
